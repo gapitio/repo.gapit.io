30a244526a9703c2841f262e58635e91525ac2e8
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-09-21 09:07:53 +0200

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>Repos</header>
+    </div>
+  );
+};
+
+export default App;
